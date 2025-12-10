6b316b6aa2cfa8bbc5925add5706c467f2069bd6
Dani Pacheco <cloudxdam@gmail.com>
2025-12-10 09:21:16 +0000

    Iniciando proyecto

---
+++ src/main/java/com/dam/api_torneo/ApiTorneoApplication.java
@@ -0,0 +1,13 @@
+package com.dam.api_torneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTorneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTorneoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/api_torneo/ApiTorneoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.api_torneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTorneoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
