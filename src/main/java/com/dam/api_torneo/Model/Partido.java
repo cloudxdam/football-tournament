bc08753d6fa4d7ed5c80e07b30689d75df74e5fd
dani-dev-2025 <danipacheco81@gmail.com>
2025-12-15 16:39:54 +0000

    correcciones en modelo Partido: se añaden los id de los equipos que juegan y la fecha

--- src/main/java/com/dam/api_torneo/Model/Partido.java
+++ src/main/java/com/dam/api_torneo/Model/Partido.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -20,6 +22,17 @@ public class Partido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPartido;
+    private String fecha;
     private String resultado;
+
+    // relación un equipo juega muchos partidos, para ver en los partidos
+    // registrados (jugados) los equipos implicados
+    @ManyToOne
+    @JoinColumn(name = "equipo_local_id")
+    private Equipo equipoLocal;
+
+    @ManyToOne
+    @JoinColumn(name = "equipo_visitante_id")
+    private Equipo equipoVisitante;
 
 }
